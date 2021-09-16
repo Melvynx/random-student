35864203168c0ad663f3a7dcf10067da628c6bf5
Melvynx <melvyn.malherbe@qoqa.com>
2021-09-16 09:27:59 +0200

    :sparkles: add spin

--- components/random/RandomModal.tsx
+++ components/random/RandomModal.tsx
@@ -6,10 +6,20 @@ import {
   DialogContent,
   Typography,
 } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
+import { Wheel } from 'react-custom-roulette';
+import { WheelData } from 'react-custom-roulette/dist/components/Wheel/types';
 
 export function getRandomValueFromArray<T>(possibilities: T[]): T {
   return possibilities[Math.floor(Math.random() * possibilities.length)];
+}
+
+const colors = ['#d63031', '#6c5ce7', '#00b894'];
+
+function getData(list: string[]): WheelData[] {
+  return list.map((v, index) => {
+    return { option: v, style: { backgroundColor: colors[index % 3] } };
+  });
 }
 
 export default function RandomModal({
@@ -20,10 +30,21 @@ export default function RandomModal({
   onFinish: () => void;
 }) {
   const random = React.useRef(getRandomValueFromArray(list));
+
+  const [mustSpin, setMustSpin] = useState(false);
+  const [prizeNumber, setPrizeNumber] = useState(0);
+
+  const handleSpinClick = () => {
+    const newPrizeNumber = Math.floor(Math.random() * list.length);
+    setPrizeNumber(newPrizeNumber);
+    setMustSpin(true);
+  };
+
   return (
     <Dialog open={true} onAbort={onFinish} onClose={onFinish}>
       <Box
         sx={{
+          overflow: 'hidden',
           display: 'flex',
           p: 2,
           flexDirection: 'column',
@@ -31,8 +52,20 @@ export default function RandomModal({
           gap: 2,
         }}
       >
-        <Typography variant="h3">{random.current}</Typography>
+        <Wheel
+          mustStartSpinning={mustSpin}
+          prizeNumber={prizeNumber}
+          data={getData(list)}
+          backgroundColors={['#3e3e3e', '#df3428']}
+          textColors={['#ffffff']}
+          onStopSpinning={() => {
+            setMustSpin(false);
+          }}
+        />
         <Button onClick={onFinish}>Close</Button>
+        <Button variant="outlined" onClick={handleSpinClick}>
+          Randomiz
+        </Button>
       </Box>
     </Dialog>
   );
