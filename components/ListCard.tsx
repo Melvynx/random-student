dfc953d73ff53f3d39ac9786eb939a63cd052663
Melvynx <melvyn.malherbe@qoqa.com>
2021-09-16 09:51:22 +0200

    :sparkles: add new images

--- components/ListCard.tsx
+++ components/ListCard.tsx
@@ -13,6 +13,12 @@ const images = [
   'assets/avatar/2.svg',
   'assets/avatar/3.svg',
   'assets/avatar/4.svg',
+  'assets/avatar/5.svg',
+  'assets/avatar/6.svg',
+  'assets/avatar/7.svg',
+  'assets/avatar/8.svg',
+  'assets/avatar/9.svg',
+  'assets/avatar/10.svg',
 ];
 console.log(images);
 
